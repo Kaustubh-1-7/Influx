66e9da915f851e192223ab8392389cade569add3
Rohangarg <garg20005rohan@gmail.com>
2025-08-02 18:17:13 +0530

    finalized the project it ias complete

--- client/src/app/page.js
+++
@@ -1,8 +0,0 @@
-import { WalletConnectButton } from "@/components/WalletConnectButton";
-import Image from "next/image";
-
-export default function Home() {
-  return (
-   <div><WalletConnectButton/></div>
-  );
-}
